a1486e0f5fc4b49a636ef2c20b357ff8d6647e43
natalianam <natalia.nam1981@gmail.com>
2019-11-08 11:20:47 -0800

    SHW15 - done

--- src/test/java/browserFactory/BrowserFactory.java
+++ src/test/java/browserFactory/BrowserFactory.java
@@ -3,13 +3,15 @@ package browserFactory;
 import enums.BrowserType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserFactory {
     public WebDriver createWebDriver(BrowserType browser) throws NoSuchMethodException {
         switch(browser){
             case CHROME:
-                return GetChroomeDriver();
+                return GetChromeDriver();
             case FIREFOX:
                 return GetFirefoxDriver();
             default:
@@ -19,11 +21,21 @@ public class BrowserFactory {
 
 
 
-    private WebDriver GetChroomeDriver() {
+    private WebDriver GetChromeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=1400,1200");
+        options.addArguments("--headless");
+
         System.setProperty("webdriver.crome.driver", "chromedriver");
         return new ChromeDriver();
     }
     private WebDriver GetFirefoxDriver() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--width=1920");
+        options.addArguments("--height=1080");
+        options.addArguments("--headless");
+        
+
         System.setProperty("webdriver.gecko.driver", "geckodriver");
         return new FirefoxDriver();
     }
